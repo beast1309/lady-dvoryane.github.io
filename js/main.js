b42b529e3b1c4434b2d9c221f349a933b1ef275c
beast1309 <56633284+beast1309@users.noreply.github.com>
2024-10-27 17:35:14 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function () {
+    wow = new WOW(
+        {
+            boxClass: 'wow',      // default
+            animateClass: 'animated', // default
+            offset: 0,          // default
+            mobile: true,       // default
+            live: true        // default
+        }
+    )
+    wow.init();
 
 });
