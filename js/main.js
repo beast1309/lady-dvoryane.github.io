3e8ce253aa3c23cead306588f8085280e89d046e
beast1309 <56633284+beast1309@users.noreply.github.com>
2024-11-09 01:25:46 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(async function () {
     wow = new WOW(
         {
             boxClass: 'wow',      // default
@@ -21,11 +21,68 @@ $(function () {
         },
     })
 
+    const cardSwiper = new Swiper('.card__swiper', {
+        slidesPerView: 5,
+        loop: true,
+        speed: 500,
+        centeredSlides: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+
     $(document.querySelectorAll('#questionBtn')).on('click', function () {
         $('.questionForm').toggleClass('--active')
     })
 
-    $('#closeBtn').on('click', function () {
+    $(document.querySelectorAll('#closeBtn')).on('click', function () {
         $('.questionForm').removeClass('--active')
+        $('.acceptForm').removeClass('--active')
     })
+
+    $(document.querySelectorAll('#acceptBtn')).on('click', function () {
+        $('.acceptForm').toggleClass('--active')
+    })
+
+    /* function loadJSON(src) {
+        return fetch(src).then(x => x.json())
+    }
+
+    const getPets = await loadJSON('./pets-db.json')
+    console.log(getPets)
+
+    class Card {
+        constructor(props = {}) {
+            this.id = props.id
+            this.name = props.name
+            this.age = props.age
+            this.race = props.race
+            this.comment = props.comment
+        }
+    } */
+
+    $(document.getElementById('petsLink').addEventListener('click', function () {
+        window.location.replace('http://localhost:3002/pets.html#')
+    }))
+
+    const petsLinks = document.querySelectorAll('.homeless__list-link')
+    console.log(petsLinks)
+
+    petsLinks.forEach((item) => {
+        /* let index = item.id
+
+        const newCard = new Card({
+            id: getPets.pets[index].id,
+            name: getPets.pets[index].name,
+            age: getPets.pets[index].age,
+            race: getPets.pets[index].race,
+            comment: getPets.pets[index].comment
+        }) */
+
+        item.addEventListener('click', function () {
+            window.location.replace('http://localhost:3002/card-1.html#')
+        })
+    })
+    console.log(petName)
 });
